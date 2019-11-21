6df2d1238ee48a16657b46638ea4cec040fd7f32
Randy Taylor <randyisbusy@gmail.com>
2019-11-21 17:54:37 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Randy", last_name: "Taylor", active: true)
